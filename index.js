5c24afb3f546dd0d518eb74ae1e975b010ea7bd8
pauliax <pauliax6@gmail.com>
2019-05-04 18:25:16 +0300

    Add output directory to config

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const loadJsonFile = require('load-json-file');
 const generator = require('./app/generator');
 
 const FILENAME_TXS = config.get('input.txs');
+const OUTPUT_DIRECTORY = config.get('output.directory');
 
 run = async function () {
   const fileContents = await loadJsonFile(FILENAME_TXS);
   const testFile = generator.generate(fileContents);
 
   const contractName = fileContents['contract'];
-  const generatedTestFile = `generated-tests/${contractName.toLowerCase()}.js`;
+  const generatedTestFile = `${OUTPUT_DIRECTORY}/${contractName.toLowerCase()}.js`;
 
   fs.writeFile(generatedTestFile, testFile, (err) => {
     if (err) {
